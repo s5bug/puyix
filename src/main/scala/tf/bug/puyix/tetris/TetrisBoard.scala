18e84e126b5644332a3760220792d86a4cfd9ccc
Anthony Cerruti <soren@disroot.org>
2018-07-27 19:32:36 -0700

    Asynchronize processMove(s)

--- src/main/scala/tf/bug/puyix/tetris/TetrisBoard.scala
+++ src/main/scala/tf/bug/puyix/tetris/TetrisBoard.scala
@@ -3,6 +3,8 @@ package tf.bug.puyix.tetris
 case class TetrisBoard(blocks: Seq[Seq[Option[TetrisBlock]]] = Seq.fill(10, 22)(None)) {
 
   def /(x: Int, y: Int): Option[TetrisBlock] = blocks(x)(y)
-  def /(c: (Int, Int)): Option[TetrisBlock] = /(c._1, c._2)
+  def /(c: (Int, Int)): Option[TetrisBlock] = c match {
+    case (x, y) => /(x, y)
+  }
 
 }
--- src/main/scala/tf/bug/puyix/tetris/TetrisSRS.scala
+++ src/main/scala/tf/bug/puyix/tetris/TetrisSRS.scala
@@ -1,13 +1,32 @@
 package tf.bug.puyix.tetris
 
+import monix.eval.Task
+
 object TetrisSRS {
+
+  type MoveState = Task[(TetrisBoard, Option[MovingTetrisPiece])]
 
   def processMoves(
     board: TetrisBoard,
     piece: MovingTetrisPiece,
     moves: Seq[TetrisMove]
-  ): (TetrisBoard, Option[MovingTetrisPiece]) = {
-    (board, Some(piece)) // TODO
+  ): Task[(TetrisBoard, Option[MovingTetrisPiece])] = Task {
+    moves.foldLeft[MoveState](Task((board, Some(piece))))((task: MoveState, nmove: TetrisMove) => {
+      task.flatMap {
+        case (nboard, npieceOption) => npieceOption match {
+          case Some(npiece) => processMove(nboard, npiece, nmove)
+          case None => Task((nboard, None))
+        }
+      }
+    })
+  }.flatten
+
+  def processMove(
+    board: TetrisBoard,
+    piece: MovingTetrisPiece,
+    move: TetrisMove
+  ): Task[(TetrisBoard, Option[MovingTetrisPiece])] = Task {
+    (board, Some(piece))
   }
 
 }
